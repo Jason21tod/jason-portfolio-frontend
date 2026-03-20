869767ebc79ab2fe182c528786453da0e8a784f5
Gian_Pereira <gian.21.otaku@gmail.com>
2026-03-20 08:23:25 -0300

    Did the hero screen on laptop

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,4 +1,5 @@
 if (window.innerWidth <= 760) {
+    console.log("switching script to fit the screen");
     document.getElementById("nav-button").addEventListener("click", handleMenuToggle);
     const menu_exit_area = document.getElementById("menu-exit-area");
     menu_exit_area.addEventListener("click", handleMenuToggle);
