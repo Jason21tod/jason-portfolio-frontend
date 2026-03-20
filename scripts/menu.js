0029fab938896ad6b1e629690e97db7cc992c9be
Gian_Pereira <gian.21.otaku@gmail.com>
2026-03-20 07:53:48 -0300

    Did the laptop screen of the header

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,26 +1,28 @@
-document.getElementById("nav-button").addEventListener("click", handleMenuToggle);
-const menu_exit_area = document.getElementById("menu-exit-area");
-menu_exit_area.addEventListener("click", handleMenuToggle);
+if (window.innerWidth <= 760) {
+    document.getElementById("nav-button").addEventListener("click", handleMenuToggle);
+    const menu_exit_area = document.getElementById("menu-exit-area");
+    menu_exit_area.addEventListener("click", handleMenuToggle);
 
 
-let menu_active = false;
-const menu_buttons = Array.from(document.getElementsByClassName("nav-component"));
+    let menu_active = false;
+    const menu_buttons = Array.from(document.getElementsByClassName("nav-component"));
 
 
-for (let i = 0; i < menu_buttons.length; i++) {
-    menu_buttons[i].addEventListener("click",  handleMenuToggle)
-}
-
-
-function handleMenuToggle() {
-    let menu = document.getElementsByClassName("header-bar")[0];
-
-    menu_active?  menu.style.right= "100%": menu.style.right = "10%";
-    if (menu_active === false) {
-        menu_active = true
-        menu_exit_area.style.display = "inline-block"
-    } else {
-        menu_active = false;
-        menu_exit_area.style.display = "None";
+    for (let i = 0; i < menu_buttons.length; i++) {
+        menu_buttons[i].addEventListener("click",  handleMenuToggle)
     }
-};
+
+
+    function handleMenuToggle() {
+        let menu = document.getElementsByClassName("header-bar")[0];
+
+        menu_active?  menu.style.right= "100%": menu.style.right = "10%";
+        if (menu_active === false) {
+            menu_active = true
+            menu_exit_area.style.display = "inline-block"
+        } else {
+            menu_active = false;
+            menu_exit_area.style.display = "None";
+        }
+    };
+}
